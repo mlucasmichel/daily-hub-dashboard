33f93acfb1b1066bbd77f2910e3fac4956b0c2ca
mlucasmichel <mlucasmichel14@gmail.com>
2025-09-20 12:28:09 +0100

    grab notes DOM elements in scripts.js

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const notesForm = document.getElementById("notes-form");
+    const notesInput = document.getElementById("notes-input");
+    const notesList = document.getElementById("notes-list");
+
+});
